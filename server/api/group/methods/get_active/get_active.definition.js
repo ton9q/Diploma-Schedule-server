d14c94175b9055718b8ddd51b783aa043391dc6c
Anton Kuchma <kuchma_anton@mail.ru>
2020-05-13 15:35:20 +0300

    feat: add queryParams=faculty,specialty for filtering groups

--- server/api/group/methods/get_active/get_active.definition.js
+++ server/api/group/methods/get_active/get_active.definition.js
@@ -3,6 +3,17 @@ module.exports = {
     path: '/active',
     verb: 'get',
   },
+  accepts: [{
+    arg: 'faculty',
+    type: 'string',
+    http: { source: 'query' },
+    description: 'Faculty name',
+  }, {
+    arg: 'specialty',
+    type: 'string',
+    http: { source: 'query' },
+    description: 'Specialty name',
+  }],
   returns: {
     type: 'array',
     root: true,
--- server/api/group/methods/get_active/get_active.method.js
+++ server/api/group/methods/get_active/get_active.method.js
@@ -1,14 +1,25 @@
 const app = require('../../../../server');
+const { getRegExp } = require('../../../../utils/regexp');
 
-module.exports = async () => {
+module.exports = async (faculty, specialty) => {
   const { Group } = app.models;
 
-  const groups = await Group.find({
+  const filter = {
     where: {
       isArchived: false,
     },
     order: 'groupName ASC',
-  });
+  };
 
-  return groups;
+  if (faculty) {
+    const facultyRegex = getRegExp(faculty, { caseSensitive: false });
+    filter.where.faculty = facultyRegex;
+  }
+
+  if (specialty) {
+    const specialtyRegex = getRegExp(specialty, { caseSensitive: false });
+    filter.where.specialty = specialtyRegex;
+  }
+
+  return Group.find(filter);
 };
